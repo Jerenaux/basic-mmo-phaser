883b9d6e9d76e3de495c1f7699b88cdefab17fb4
Jerome <jerome.renaux@gmail.com>
2017-06-18 23:16:11 +0200

    Client-side, moved the 'move' callback declaration within the 'allplayers' declaration

--- js/client.js
+++ js/client.js
@@ -26,11 +26,13 @@ Client.socket.on('allplayers',function(data){
     for(var i = 0; i < data.length; i++){
         Game.addNewPlayer(data[i].id,data[i].x,data[i].y);
     }
+
+    Client.socket.on('move',function(data){
+        Game.movePlayer(data.id,data.x,data.y);
+    });
 });
 
-Client.socket.on('move',function(data){
-    Game.movePlayer(data.id,data.x,data.y);
-});
+
 
 Client.socket.on('remove',function(id){
     Game.removePlayer(id);
